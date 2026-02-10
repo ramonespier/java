bf386056febdc97d6be18c2db8e0151a8f3defee
ramonespier <ramon.coelhomelo@outlook.com>
2026-02-10 15:00:55 -0300

    interfaces e classes abstratas

--- java_pack/Carro.java
+++ java_pack/Carro.java
@@ -1,24 +1,7 @@
 package java_pack;
 
-public class Carro {
-    String marca;
-
-    public static void main(String[] args) {
-        System.out.println("testeee");
-    }
-
-    public Carro(String marca) {
-        this.marca = marca;
-    }
-
-    protected void acelerar() {
-        System.out.println("Acelerando o " + this.marca);
-    }
-}
-
-class Rodas {
-    public Rodas () {
-        Carro carro = new Carro("Renault");
-        carro.acelerar();
-    }
+public interface Carro {
+    void acelerar();
+    void freiar();
+    void parar();
 }
